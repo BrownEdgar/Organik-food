8931f7efae97e2e1c02bd18371ab364868821739
ghostmeta <hwkunique@gmail.com>
2023-11-09 17:30:33 +0400

    90% of Products

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
-import './Home.scss'
+import Products from "../../components/Products/Products";
+import "./Home.scss";
 
 export default function Home() {
   return (
     <div>
       <h1>Home page</h1>
+      <Products />
     </div>
-  )
+  );
 }
