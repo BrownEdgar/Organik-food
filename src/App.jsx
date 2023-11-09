a8dcb510962f8cab448f5d6d402cb960dffa0251
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-11-09 14:23:18 +0400

    navbar part 1 ondone

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Layouts from './components/Layouts/Layouts'
-import Home from './pages/Home/Home'
+import { About, Home } from './pages'
 import { Route, RouterProvider, createRoutesFromElements, createBrowserRouter } from 'react-router-dom'
+import ROUTES from './routes/routes'
 
 export default function App() {
 
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path='/' element={<Layouts />}>
+      <Route path={ROUTES.HOME} element={<Layouts />}>
         <Route index element={<Home />} />
+        <Route path={ROUTES.ABOUT} element={<About />} />
       </Route>
     )
   )
--- src/features/productSlice.js
+++ src/features/productSlice.js
@@ -1,6 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = []
+const initialState = [
+
+]
 const productsSlice = createSlice({
   name: 'products',
   initialState: initialState,
