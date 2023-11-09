c383fd8ef7e9a79ae6574c5dbb6327febfdddd2e
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-11-09 13:17:30 +0400

    added redux folders

---
+++ src/features/productSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = []
+const productsSlice = createSlice({
+  name: 'products',
+  initialState: initialState,
+  reducers: {}
+})
+
+
+export default productsSlice.reducer
