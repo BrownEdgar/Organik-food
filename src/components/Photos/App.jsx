89ab30f4a9f5c7dd1bdb9942a6d4272325407a41
Levon0117 <143790164+Levon0117@users.noreply.github.com>
2023-11-10 13:35:04 +0400

    features/7005 is done

---
+++ src/components/Photos/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='body'>
+      <div className='body-container'>
+         <div className='juice'>
+          <img src="./photo/juice.png" alt="" />
+          <div className='title1'>
+            <p className='p1'>Organic Juice</p>
+          </div>
+         </div>
+         <div className='food'>
+          <img src="./photo/food.png" alt="" />
+          <div className='title2'>
+            <p className='p2'>Organic Food</p>
+          </div>
+         </div>
+         <div className='nuts'>
+          <img src="./photo/nuts.png" alt="" />
+          <div className='title3'>
+            <p className='p3'>Nuts Cookis</p>
+          </div>
+         </div>
+      </div>
+    </div>
+  )
+}
